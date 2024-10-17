d5740503f63ed88859694af77853be7a60ad72fd
adi3489 <07aditya.cn@gmail.com>
2024-10-17 12:34:58 +0530

    palindromeNumber

--- src/palindromeNumber.java
+++ src/palindromeNumber.java
@@ -18,7 +18,7 @@ if(originalNumber==reverseNumber){
 
     }
     public static void main(String[] args) {
-
+        System.out.println("Hello world");
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the number");
 
