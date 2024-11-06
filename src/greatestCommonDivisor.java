14315516374b0b27a898adc945e1ec716f72c39a
adi3489 <07aditya.cn@gmail.com>
2024-11-06 19:08:44 +0530

    gcd

---
+++ src/greatestCommonDivisor.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class greatestCommonDivisor {
+
+    public static int GCD(int num1,int num2) {
+int result = Math.min(num1,num2);
+while(result>0){
+    if(num1%result==0 && num2%result==0){
+        break;
+    }
+    result--;
+}
+
+return result;
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number");
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+
+        int HCF = GCD(num1,num2);
+        System.out.println(HCF);
+    }
+}
