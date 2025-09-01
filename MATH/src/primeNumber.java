91e375950b76f29932c2c40f2684fb0107f624ae
adi3489 <07aditya.cn@gmail.com>
2025-09-01 12:06:08 +0530

    primeNumber

--- MATH/src/primeNumber.java
+++ MATH/src/primeNumber.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class primeNumber {
 
+    public static boolean checkPrime(int a ) {
+        if (a <= 1) return false;
+        boolean isprime = true;
+
+        for (int i = 2; i <= a / 2; i++) {
+
+            if (a % i == 0) {
+                isprime=false;
+            }
+        }
+        return isprime;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println(checkPrime(a));
+    }
 }
