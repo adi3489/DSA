73327a8f6c34a3de339d47b22dc2df42d426150b
adi3489 <07aditya.cn@gmail.com>
2025-10-02 18:50:11 +0530

    All divisor of a number

---
+++ MATH/src/allDivisorNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class allDivisorNumber {
+    public static void main(String[] args) {
+
+
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            if (x % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
